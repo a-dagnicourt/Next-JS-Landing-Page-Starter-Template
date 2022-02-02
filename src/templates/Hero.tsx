8e51f2a6fdb9d71f93390b3effd8e87261b93059
a.dagnicourt <dagnicourt.a@gmail.com>
2022-02-02 10:37:16 +0100

    style: add mist effec to to hero

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link';
 import { HiChevronDoubleDown } from 'react-icons/hi';
 
+import { Fog } from '../components/Fog';
 import { AppConfig } from '../utils/AppConfig';
 
 const Hero = () => (
   <header className="h-screen relative bg-hero-pattern bg-center bg-cover">
+    <Fog />
     <div className="absolute bg-black/75 w-full h-full animate-pulse-slow" />
     <div className="absolute w-full h-full flex flex-col justify-around items-center mx-auto px-3 py-6 text-center">
       <div>
-        <h1 className="text-6xl text-red-900 font-bold whitespace-pre-line leading-hero font-title">
+        <h1 className="text-4xl md:text-6xl text-red-900 font-bold whitespace-pre-line leading-hero font-title">
           {AppConfig.site_name}
         </h1>
-        <p className="text-xl">
+        <p className="text-lg md:text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
       </div>
